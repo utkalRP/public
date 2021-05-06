41e9adc047f7c99c2a12c2b314f5d56a0e147da0
utkalRP <urpradhan@gmail.com>
2021-05-06 10:01:38 +0530

    Update Vaccination.py

--- Vaccination.py
+++ Vaccination.py
@@ -8,18 +8,19 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 pins = ['500019']
 
 #Enter for how many days you want to search from today
-advDay = 10
+advDay = 30
 
 st = datetime.now()
-header = {'Accept-Language': 'hi_IN', 'accept': 'application/json'}
+header = {'Accept-Language': 'hi_IN', 'accept': 'application/json', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
 
 for pin in pins:
     for i in range(0, advDay+1):
         day = (st + timedelta(days = i)).strftime('%d-%m-%Y')
-        url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=' + pin + '&date=' + day
-        res = requests.get(url, params={}, headers=header, verify=False)
+        param = {'pincode':pin, 'date':day}
+        url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin'
+        res = requests.get(url, params=param, headers=header, verify=False)
         resList = json.loads(res.text)
         if len(resList['sessions']) == 0:
-            print(f'No slots available for PIN: {pin} and Date: {day}')
+            print('No slots available for PIN: {} and Date: {}'.format(pin, day))
         for s in resList['sessions']:
-            print(f'Avilable slot on {day}:\nAddress:\t{s["name"]}, {s["address"]}, {s["block_name"]}, {s["district_name"]}, {s["state_name"]}\nSlots:\t\t{s["slots"]}\nFee Type:\t{s["fee_type"]}\nFee:\t\t{s["fee"]}\nCapacity:\t{s["available_capacity"]}\nMin Age:\t{s["min_age_limit"]}\nVaccine:\t{s["vaccine"]}\n')
+            print('Avilable slot on {}:\nAddress:\t{}, {}, {}, {}, {}\nSlots:\t\t{}\nFee Type:\t{}\nFee:\t\t{}\nCapacity:\t{}\nMin Age:\t{}\nVaccine:\t{}\n'.format(day, s["name"], s["address"], s["block_name"], s["district_name"], s["state_name"], s["slots"], s["fee_type"], s["fee"], s["available_capacity"], s["min_age_limit"], s["vaccine"]))
