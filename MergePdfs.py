ac9b56e631467551876c2e61663391e0a149b518
utkalRP <urpradhan@gmail.com>
2021-05-11 11:39:22 +0530

    Delete MergePdfs.py

--- MergePdfs.py
+++
@@ -1,7 +0,0 @@
-from PyPDF2 import PdfFileMerger, PdfFileReader
-
-mergerObject = PdfFileMerger()
-for i in range(99,119):
-    mergerObject.append(PdfFileReader(f'hhvs{i}.pdf', 'rb'))
-
-mergerObject.write('MergedFile.pdf')
